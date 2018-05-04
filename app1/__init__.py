7cf65b5ebe1ecc2e661ed2796dc96ee1531308c5
krupnikola <nikola@example.com>
2018-05-04 09:11:51 +0200

    ch. 6

--- app1/__init__.py
+++ app1/__init__.py
@@ -5,11 +5,12 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-# protection of login required views
+# protection of login required views, here is specified what route to trigger if user is not authenticated
 login.login_view = 'login'
+
 from app1 import routes, models
